427f4df79975ade7f11896526205ac1920840cef
eddie_liu <eddie_liu@htc.com>
2016-02-25 18:22:56 +0800

    first commit

---
+++ modules/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Home from './Home'
+import NavLink from './NavLink'
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Ghettohub Issues</h1>
+        <ul role="nav">
+          <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/repos">Repos</NavLink></li>
+        </ul>
+
+        {/* add this */}
+        {this.props.children}
+
+      </div>
+    )
+  }
+})
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './index.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: 'public'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+    ]
+  },
+  
+  plugins: process.env.NODE_ENV === 'production' ? [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ] : [],
+}
